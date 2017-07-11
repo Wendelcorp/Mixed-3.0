f3e6ee17c7a255d24f531524e8010f666103e66e
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-07-11 13:02:49 -0400

    Updates routes with more accurate names

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :mixes do
-    resources :songs
+  resources :mixes, :path => 'collection' do
+    resources :songs, :path => 'mixtape'
   end
   root to: 'mixes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
