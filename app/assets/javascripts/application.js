21284cf3516278ebde33babee9e20b807ade1e1d
Wendelcorp <bryce@brycewendelaar.com>
2017-08-02 01:12:29 -0400

    CSS updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require turbolinks
 //= require jssocials.min.js
 //= require_tree .
+
+$(document).ready(function(){
+  var stars=600;
+  var $stars=$(".stars");
+  var r=800;
+  for(var i=0;i<stars;i++){
+    var $star=$("<div/>").addClass("star");
+    $stars.append($star);
+  }
+  $(".star").each(function(){
+    var cur=$(this);
+    var s=0.2+(Math.random()*1);
+    var curR=r+(Math.random()*200);
+    cur.css({
+      transformOrigin:"0 0 "+curR+"px",
+      transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+
+    })
+  })
+})
